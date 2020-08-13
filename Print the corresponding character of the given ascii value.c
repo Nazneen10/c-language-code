73c3c9582f764905e81de833e4ba641cba9f210b
Nazneen10 <69063553+Nazneen10@users.noreply.github.com>
2020-08-13 21:59:56 +0530

    Create Print the corresponding character of the given ascii value.c

---
+++ Print the corresponding character of the given ascii value.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+//Compiler version gcc  6.3.0
+
+int main()
+{ 
+  int s=98;
+  char k='c';
+  char p;
+  int l;
+  
+  p=(char)s;
+  l=(int)k;
+  
+  
+  printf("%c",p);
+  printf("\n%d",l);
+  return 0;
+}
